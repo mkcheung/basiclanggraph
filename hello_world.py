a528b58a22077ddf9b8ee97818945fcfac495402
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-07-17 23:14:10 -0700

    create linear graph workflow

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,26 @@ from typing import TypedDict
 from langgraph.graph import END, START, StateGraph
 from util.langgraph_util import display
 
+class HelloWorldState(TypedDict):
+    message: str
+
+def hello(state: HelloWorldState):
+    print(f"Hello Node: {state['message']}")
+    return {"message": "Hello "+state['message']}
+
+def bye(state: HelloWorldState):
+    print(f"Bye Node: {state['message']}")
+    return {"message": "Bye "+state['message']}
+
+graph = StateGraph(HelloWorldState)
+
+graph.add_node("hello", hello)
+graph.add_node("bye", bye)
+
+graph.add_edge(START, "hello")
+graph.add_edge("hello", "bye")
+graph.add_edge("bye", END)
+
+runnable = graph.compile()
+output = runnable.invoke({"message": "Mars"})
+print(output)
